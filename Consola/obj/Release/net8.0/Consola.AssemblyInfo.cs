dc2fea4505b8e038cd9b9eef7cba5bc0dd6a19d0
dianatecheira <dianatecheira@outlook.com>
2025-04-22 14:19:45 -0300

    feat: Ignorar archivos de Visual Studio

--- Consola/obj/Release/net8.0/Consola.AssemblyInfo.cs
+++ Consola/obj/Release/net8.0/Consola.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consola")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dacaf0a3c523a5b36cc6ef83450b4308438c1dc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consola")]
